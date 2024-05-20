707279b10fb6039e175ebf00a0f2b2ded9e3025b
Athul pk <athulprathap96@gmail.com>
2024-05-20 19:05:52 +0530

    feature: backend io-socket implimented

---
+++ Backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const http = require("http");
+
+const cors = require("cors")
+const app = express();
+const port = 9000;
+
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "https://localhost:5174",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
+app.use(cors(
+    {
+        origin: "https://localhost:5174",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+))
+
+io.on("connection", (socket) => {
+    console.log("user connected");
+    console.log("id", socket.id);
+});
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
